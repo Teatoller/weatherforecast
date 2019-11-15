379c9bebe3ca89783fd5ae0e34d6b3b06e363b1e
Teatoller <steven.otieno1@gmail.com>
2019-11-15 22:21:53 +0300

    (API fetch): Implement project setup
    - clear default files
    - fetch weather api endpoint
    
    [Starts #002]

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      coord: {},
+      weather: [],
+      base: "",
+      main: {},
+      wind: {},
+      rain: [],
+      clouds: {},
+      dt: 0,
+      sys: {}
+    };
+  }
+  // "$apiUrl?lon={$longitude}&lat={$latitude}"
+  componentDidMount() {
+    let latitude = 35;
+    let longitude = 159;
+    fetch(
+      `https://fcc-weather-api.glitch.me//api/current?lat=${latitude}&lon=${longitude}`
+    )
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({
+          coord: responseData.coord,
+          weather: responseData.weather,
+          base: responseData.base,
+          main: responseData.main,
+          wind: responseData.wind,
+          rain: responseData.rain,
+          clouds: responseData.clouds,
+          dt: responseData.dt,
+          sys: responseData.sys
+        });
+      })
+      .catch(error => {
+        console.log("Error fetching and parsing data", error);
+      });
+  }
+
+  render() {
+    console.log(this.state.coord);
+    console.log(this.state.weather);
+    console.log(this.state.base);
+    console.log(this.state.main);
+    console.log(this.state.wind);
+    console.log(this.state.rain);
+    console.log(this.state.clouds);
+    console.log(this.state.dt);
+    console.log(this.state.sys);
+
+    return <h1>Weather</h1>;
+  }
 }
 
 export default App;
